ebf4d67806e73a5354eaa7d33c01a329b784e799
Madhu-Mitha <Madhu_Mitha@Dell.com>
2024-06-24 13:43:03 +0530

    react can be used in a small part of app

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!")
+const heading1 = React.createElement("h1", {id: "heading1"}, "heading1")
+const heading2 = React.createElement("h1", {id: "heading2"}, "heading2")
+const parent = 
+    React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, [heading1, heading2]))
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+console.log(parent)
+root.render(parent)
